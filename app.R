31266d4f305565abecb6ae5c4d0152ce2a69bee2
xavier-chenede <x_chenede@yahoo.com>
2022-12-17 17:01:17 +0100

    Initialisation

---
+++ app.R
@@ -0,0 +1,24 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+        
+        "Hello Shiny!"
+        
+)
+        
+server <- function(input, output) {
+                
+        }
+        # Run the application 
+shinyApp(ui = ui, server = server)
