8e5ce18cf5391c6096da5d247f6e23fa0f9eacd5
xavier-chenede <x_chenede@yahoo.com>
2022-12-18 20:50:06 +0100

    Button works ;)... only once :(

--- app.R
+++ app.R
@@ -15,12 +15,34 @@ ui <- fluidPage(
         
         h1("Hello Shiny!"),
         hr(),
-        h2("h2 format")
+        h2("h2 format"),
+        hr(),
         
+        actionButton(inputId="but1",
+                     label="Add +1"),
+        
+        textOutput('result')
 )
         
+
 server <- function(input, output) {
-                
-        }
+        
+        x <- 0
+        clic <- eventReactive(input$but1, {
+                x <- x+1
+        }, ignoreNULL = FALSE)  
+        
+        
+        
+        observe({
+                if(input$but1 > 0){
+                        output$result=clic
+                        }
+        })
+        
+
+}
+
         # Run the application 
 shinyApp(ui = ui, server = server)
+
