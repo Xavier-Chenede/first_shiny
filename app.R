77443e511fa4add17c3cee6b32dcc0a299a8c229
xavier-chenede <x_chenede@yahoo.com>
2022-12-20 17:13:42 +0100

    add an auto-reset

--- app.R
+++ app.R
@@ -29,28 +29,38 @@ x <- 0
 
 server <- function(input, output) {
         
-        click <- eventReactive(input$but1,{
-                if (x==100) {x<<-1} else {x <<- x+1} #resetting if 100th click
-                })
-        
-        click_rz <- eventReactive(input$but2,{
-                x<<-1 #resetting at click
-                })
-        
-        # output$result <- renderText({
-        #         click()
+        # click <- eventReactive(input$but1,{
+        #         if (x==100) {x<<-1} else {x <<- x+1} #resetting if 100th click
         #         })
-        
-        output$result <- renderText({
-                if (input$but2>0) {click_rz()} else {click}
-                })
-        
-    
-        
-        
-}       
+        # 
+        # click_rz <- eventReactive(input$but2,{
+        #         x<<-1 #resetting at click
+        #         })
+        # 
+        # # output$result <- renderText({
+        # #         click()
+        # #         })
+        # 
+        # output$result <- renderText({
+        #         if (input$but2>0) {click_rz()} else {click}
+        #         })
 
-        # Run the application 
+        v <- reactiveValues(data = x)
+    
+            observeEvent(input$but1, {
+                v$data<-v$data+1
+            })
+            
+            observeEvent(input$but2, {
+                v$data<-0
+            })  
+            
+            output$result <- renderText({
+                v$data 
+            })
+        }
+        
+                # Run the application 
 shinyApp(ui = ui, server = server)
 
 
